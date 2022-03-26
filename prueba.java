a3c417839c8a18e3e162a6d49a44b285dc63c99c
Ruben <rubenfa46@educastur.es>
2022-03-26 16:35:45 +0100

    Segundo commit

--- prueba.java
+++ prueba.java
@@ -1,5 +1,6 @@
 public class Prueba{
 	public static void main(String[] args){
 		System.out.println("Eyy muy buenas a todos guapisimos");
+		System.out.println("HOLA HOLA HOLA Q TAL CRAKCS");
 	}
 }
